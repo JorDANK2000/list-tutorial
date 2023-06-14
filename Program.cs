2eed34b9523656c21cacdb8267f56d0da21a4a99
Jordan King <113210866+JorDANK2000@users.noreply.github.com>
2023-06-14 16:49:26 -0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿//Microsoft list tutorial completed by Jordan King on 06-05-2023
+
+WorkWithStrings();
+
+var fibonacciNumbers = new List<int> {1, 1};
+
+var prev = fibonacciNumbers[fibonacciNumbers.Count - 1];
+var prev2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
+
+fibonacciNumbers.Add(prev + prev2);
+
+foreach (var item in fibonacciNumbers)
+{
+    Console.WriteLine(item);
+}
+
+void WorkWithStrings(){
+    var names = new List<string> {"Jordan", "Ana","Felipe"};
+    foreach (var name in names){
+        Console.WriteLine($"Hello {name.ToUpper()}");
+    }
+    Console.WriteLine();
+    names.Add("Maria");
+    names.Add("Bill");
+    names.Remove("Ana");
+    foreach (var name in names){
+        Console.WriteLine($"Hello {name.ToUpper()}");
+    }
+    Console.WriteLine($"My name is {names[0]}");
+    Console.WriteLine($"I've added {names[2]} and {names[3]} to the list");
+    Console.WriteLine($"The list has {names.Count} people in it");
+    var index = names.IndexOf("Felipe");
+    if (index == -1)
+    {
+        Console.WriteLine($"When an item is not found, IndexOf returns {index}");
+    }
+    else
+    {
+        Console.WriteLine($"The name {names[index]} is at index {index}");
+    }
+
+    index = names.IndexOf("Not Found");
+    if (index == -1)
+    {
+        Console.WriteLine($"When an item is not found, IndexOf returns {index}");
+    }
+    else
+    {
+        Console.WriteLine($"The name {names[index]} is at index {index}");
+
+    }
+    names.Sort();
+    foreach (var name in names)
+    {
+        Console.WriteLine($"Hello {name.ToUpper()}!");
+    }
+}
